f7ea1d0588ed24d2216dd357327fb3e54e91c409
castorichy <castorichy@gmail.com>
2022-10-18 18:10:25 +0300

    editted files

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _putchar - whrites a character
+ * @s: character
+ *
+ * Return: null
+ */
+
+void _putchar(char s)
+{
+	putchar(s);
+}
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+int _printf(const char *format, ...);
+void _putchar(char s);
+
+#endif /* MAIN_H */
