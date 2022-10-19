8278c4c18998c3e621c4de315fd398d4c470306b
castorichy <castorichy@gmail.com>
2022-10-20 00:21:11 +0300

    initial commits

--- _printf.c
+++ _printf.c
@@ -1,75 +1,23 @@
 #include "main.h"
 
 /**
- * check_specifier - checks for a valid specifier
- * @format: specifier passed
+ *_printf - prints to the output
  *
- * Return: pointer to valid fanction
- */
-
-int (*check_specifier(const char *format))(va_list)
-{
-	unsigned int i;
-
-	print s[] = {{"c", print_c},
-				{"s", print_s},
-				/*{"i", print_i },
-				{"d", print_d},*/
-				{NULL, NULL}
-	};
-
-	for (i = 0; s[i].va != NULL; i++)
-	{
-		if (*(s[i].va) == *format)
-		{
-			break;
-		}
-	}
-	return (s[i].f);
-}
-
-/**
- *_printf- function that converts format specifiers
- *@format: string to be passed
- *Return: length of characters
+ *@format: pointer
+ *
+ *Return: input len
+ *
  */
 
 int _printf(const char *format, ...)
 {
-	unsigned int j, len = 0;
-	va_list ap;
-	int (*f)(va_list);
+	va_list arg;
+	int done;
 
-	if (format == NULL)
-		return (-1);
+	va_start(arg, format);
 
-	va_start(ap, format);
-	while (format[j])
-	{
-		for (j = 0; format[j] != '%' && format[j]; j++)
-		{
-			_putchar(format[j]);
-			len++;
-		}
-		if (!format[j])
-			return (len);
-		f = check_specifier(&format[j + 1]);
-		if (f != NULL)
-		{
-			len += f(ap);
-			j += 2;
-			continue;
-		}
-		if (!format[j + 1])
-			return (-1);
-		_putchar(format[j]);
-		len++;
-		if (format[j + 1] == '%')
-			j += 2;
-		else
-			j++;
-	}
-	va_end(ap);
-	return (len);
+	done = vfprintf(stdout, format, arg);
+
+	return (done);
 
 }
--- main.h
+++ main.h
@@ -1,28 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-/**
- *struct p- holds format specifiers
- *@va: type to print
- *@f: funtion to print
- */
-typedef struct p
-{
-		char *va;
-		int (*f)(va_list);
-}print;
+
+
+#include <stdio.h>
+#include <stdarg.h>
 
 int _printf(const char *format, ...);
-int (*check_for_specifiers(const char *format))(va_list);
-void _putchar(char s);
-int print_c(va_list c);
-int print_s(va_list s);
-int print_d(va_list d);
-int print_i(va_list i);
 
-
-#endif /* MAIN_H */
+#endif
