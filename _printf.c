a1e558ab9a4679b1fd6135bf29fa28d6f61b523f
castorichy <castorichy@gmail.com>
2022-10-19 18:00:27 +0300

    initial commits

---
+++ _printf.c
@@ -0,0 +1,73 @@
+#include "main.h"
+
+/**
+ * check_specifier - checks for a valid specifier
+ * @format: specifier passed
+ *
+ * Return: pointer to valid fanction
+ */
+
+static int (*check_specifier(const char *format)(va_list))
+{
+	unsigned int i;
+
+	print s[] = {{"c", print_c},
+				{"s", print_s},
+				{"i", print_i },
+				{"d", print_d},
+				{NULL, NULL}
+	};
+
+	for (i = 0; s[i].va != '\0'; i++)
+	{
+		if (s[i]->va ==*format)
+		{
+			break;
+		}
+	}
+	return (s[i].f);
+}
+
+/**
+ *
+ */
+
+int _printf(const char *format, ...)
+{
+	unsigned int i, j, len = 0;
+	va_list ap;
+	int (*f)(va_list);
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(ap, format);
+	for (i = 0; format[i] != NULL; i++)
+	{
+		for (j = 0; format[j] != '%' && format[j]; j++)
+		{
+			_putchar(format[i]);
+			len++;
+		}
+		if (!format[j])
+			return (len);
+		f = check_specifier(&format[j + 1]);
+		if (f != NULL)
+		{
+			len += f(ap);
+			j += 2;
+			continue;
+		}
+		if (!format[j + 1])
+			return (-1);
+		_putchar(format[j]);
+		len++;
+		if (format[j + 1] == '%')
+			j += 2;
+		else
+			i++;
+	}
+	va_end(ap);
+	return (len);
+
+}
