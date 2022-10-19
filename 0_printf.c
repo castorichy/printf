a5f8a629f586c38d4e52fdff0e9bba64ce41cb63
castorichy <castorichy@gmail.com>
2022-10-19 15:50:24 +0300

    more functions added

--- 0_printf.c
+++ 0_printf.c
@@ -2,18 +2,15 @@
 
 /**
  * _printf - send string to stdout
- *
  * @format: string passed
  *
  * Return: return length
  */
 
-
 int _printf(const char *format, ...)
 {
 	va_list print;
 	int i, j = 0, len = 0;
-	char ch, *str;
 
 	va_start(print, format);
 	if (*format)
@@ -29,18 +26,11 @@ int _printf(const char *format, ...)
 			switch (format[i + 1])
 			{
 				case 'c':
-					ch = va_arg(print, int);
-					_putchar(ch);
+					len += charc(va_arg(print, int));
 					i++;
-					len++;
 					break;
 				case 's':
-					str = va_arg(print, char *);
-					while (str[j])
-					{
-						_putchar(str[j]);
-						j++;
-					}
+					len += string(va_arg(print, char *));
 					i++;
 					break;
 				case '%':
@@ -59,3 +49,51 @@ int _printf(const char *format, ...)
 	va_end(print);
 	return (len - 1 + j);
 }
+/**
+ * length - calculates the lenght of a string
+ * @str: string
+ *
+ * Return: length
+ */
+int length(char *str)
+{
+	int i, len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * charc - prints a char
+ *
+ * @a: character
+ *
+ * Return: returns len;
+ */
+int charc(char a)
+{
+	_putchar(a);
+	return (1);
+}
+
+/**
+ * string - prints a string
+ *
+ * @str: string
+ * Return: return length
+ */
+
+int string(char *str)
+{
+	int i, l = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	l = length(str);
+	return (l);
+}
+
+
